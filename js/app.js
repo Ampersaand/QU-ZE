4257ba89d56dacc5735a1329f27fdb3dbd0c5307
Ampersaand <shvorobvladislav345@gmail.com>
2022-03-05 15:49:06 +0300

    Locer
    Locker

---
+++ js/app.js
@@ -0,0 +1,67 @@
+/* Устанавливаем стартовый индекс слайда по умолчанию: */
+let slideIndex = 1;
+/* Вызываем функцию, которая реализована ниже: */
+showSlides(slideIndex);
+
+/* Увеличиваем индекс на 1 — показываем следующий слайд: */
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
+function previousSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+/* Устанавливаем текущий слайд: */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Функция перелистывания: */
+function showSlides(n) {
+    /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
+    let slides = document.getElementsByClassName("item");
+
+    /* Проверяем количество слайдов: */
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    /* Проходим по каждому слайду в цикле for: */
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    /* Делаем элемент блочным: */
+    slides[slideIndex - 1].style.display = "block";
+}
+
+/* burger */
+
+const burger = document.querySelector('.burger')
+const nav = document.querySelector('.nav')
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('show')
+})
+
+/* smooth-scroll-link */
+
+const smoothScrollLinks = document.querySelectorAll('.smooth-scroll-link')
+
+smoothScrollLinks.forEach(item => {
+    item.addEventListener('click', Event => {
+        Event.preventDefault()
+
+        const target = document.querySelector(`${item.getAttribute('href')}`)
+        nav.classList.remove('show')
+
+        window.scrollBy({
+            top: target.getBoundingClientRect().top,
+            behavior: 'smooth'
+        })
+    })
+})
